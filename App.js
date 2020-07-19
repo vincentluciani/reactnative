98e99a687ba255549c132da6b4de36fb7592ad1e
Vincent LUCIANI <SESA201795@se.com>
2020-07-19 15:26:07 +0100

    initial commit - a little bit more than helloworld

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Alert,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  let x = 1;
+  const [outputText, setOutputText] = useState(
+    "Hello Mister Vincent ! How is life now in Warsaw, Poland ?"
+  );
+  const Separator = () => <View style={styles.separator} />;
+  const handlePress = () => console.log("text pressed");
+  console.log("test in progress sir");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        {outputText}
+      </Text>
+      <Separator />
+      <Image source={require("./assets/icon.png")} style={styles.logo} />
+      <Separator />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          setOutputText("Very well, thank you Vincent sir");
+          Alert.alert("Button pressed");
+        }}
+      >
+        <Text style={styles.buttonTitle}>Button</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +44,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+  button: {
+    backgroundColor: "#788fdf",
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  buttonTitle: {
+    fontSize: 40,
+    fontWeight: "400",
+    color: "#fff",
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: "#737373",
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
